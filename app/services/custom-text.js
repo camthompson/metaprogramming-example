3fd897ee38ea1c3d740f96c2c4a170bb8bddb932
Cam Thompson <cam@camthompson.com>
2015-09-02 16:28:40 -0400

    Allow overriding defaults

--- app/services/custom-text.js
+++ app/services/custom-text.js
@@ -1,12 +1,30 @@
 import Ember from 'ember';
 
 const {
+  computed,
   Service
 } = Ember;
 
 export default Service.extend({
-  siteName: 'My Cool Site',
-  aboutMe: "I'm cool, and so is this site!",
-  greeting: 'Welcome!',
-  contactMe: 'Leave me alone, please'
+  customTexts: [],
+
+  siteName: computed(function() {
+    const custom = this.get('customTexts').findBy('key', 'siteName');
+    return custom ? custom.get('value') : 'My Cool Site';
+  }),
+
+  aboutMe: computed(function() {
+    const custom = this.get('customTexts').findBy('key', 'aboutMe');
+    return custom ? custom.get('value') : "I'm cool, and so is this site!";
+  }),
+
+  greeting: computed(function() {
+    const custom = this.get('customTexts').findBy('key', 'greeting');
+    return custom ? custom.get('value') : 'Welcome!';
+  }),
+
+  contactMe: computed(function() {
+    const custom = this.get('customTexts').findBy('key', 'contactMe');
+    return custom ? custom.get('value') : 'Leave me alone, please';
+  })
 });
--- tests/unit/services/custom-text-test.js
+++ tests/unit/services/custom-text-test.js
@@ -1,6 +1,11 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleFor('service:custom-text');
+
+function create() {
+  return Ember.Object.create(...arguments);
+}
 
 test('It has default values', function(assert) {
   const service = this.subject();
@@ -8,4 +13,19 @@ test('It has default values', function(assert) {
   assert.equal(service.get('aboutMe'),  "I'm cool, and so is this site!");
   assert.equal(service.get('greeting'),  'Welcome!');
   assert.equal(service.get('contactMe'), 'Leave me alone, please');
+});
+
+test('Defaults can be overridden', function(assert) {
+  const service = this.subject(create({
+    customTexts: [
+      create({ key: 'siteName', value: 'This Site Is Cooler' }),
+      create({ key: 'aboutMe', value: "I'm not cool, but this site sure is" }),
+      create({ key: 'greeting', value: 'Sup' }),
+      create({ key: 'contactMe', value: 'Hit me up on my pager' })
+    ]
+  }));
+  assert.equal(service.get('siteName'), 'This Site Is Cooler');
+  assert.equal(service.get('aboutMe'),  "I'm not cool, but this site sure is");
+  assert.equal(service.get('greeting'),  'Sup');
+  assert.equal(service.get('contactMe'), 'Hit me up on my pager');
 });
